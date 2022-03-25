b6c48a6c2e30d6db8244e1f679e98bca6d17f050
Elterror <71641771+Elterror@users.noreply.github.com>
2022-03-25 15:14:32 +0100

    Read Products from Csv and Upload to Database Enum Added

--- app/src/main/java/com/example/pangerlular/MainActivity.java
+++ app/src/main/java/com/example/pangerlular/MainActivity.java
@@ -4,15 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.opencsv.CSVReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         DBManager db = new DBManager();
-        db.writeToDatabase();
+        db.initProducts(productsFromCsv());
 
+    }
+
+    private List<Product>  productsFromCsv() {
+        List<Product> products = new ArrayList<>();
+        try {
+            CSVReader reader = new CSVReader(new InputStreamReader(getResources().openRawResource(R.raw.products)));
+            String[] nextLine;
+            while((nextLine = reader.readNext()) != null)  {
+                nextLine = nextLine[0].split(";");
+                products.add( new Product( nextLine[0], ETypen.valueOf(nextLine[1]), Double.parseDouble(nextLine[2])));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("File Not Found");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IO Exception");
+        }
+        return products;
     }
 }

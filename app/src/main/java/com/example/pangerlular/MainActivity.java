2d9f47a7442ee2ba6bb35118d64e9bc9b5f4d705
Elterror <71641771+Elterror@users.noreply.github.com>
2022-04-22 14:29:00 +0200

    Ortner: Main-Activity

--- app/src/main/java/com/example/pangerlular/MainActivity.java
+++ app/src/main/java/com/example/pangerlular/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pangerlular;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.opencsv.CSVReader;
@@ -25,6 +26,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         DBManager db = new DBManager();
 
         db.initProducts(new InputStreamReader(getResources().openRawResource(R.raw.products)));
-        listView = findViewById(R.id.listView_products);
+        listView = findViewById(R.id.ListViewProducts);
 
         db.addCustomer(new Customer(0, "Hans", "Peter", "hpeter", "12345", "h.@gmail", new Address("Erdbeerstrasse 2", 4070, "Eferding"), new Cart()));
         db.addCustomer(new Customer(1, "Franz", "Kunsti", "fkunsti", "12345", "f.@gmail", new Address("Erdbeerstrasse 2", 4070, "Eferding"), new Cart()));
@@ -41,8 +43,17 @@ public class MainActivity extends AppCompatActivity {
         List<Customer> customers = db.getCustomers();
         List<Product> products = db.getProducts();
 
+        List<String> productsViewList = new ArrayList<>();
+        for (Product product :
+                products) {
+            productsViewList.add(product.getName());
+        }
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,productsViewList);
+        listView.setAdapter(arrayAdapter);
+
         //final ListView list = findViewById(R.id.list);
-        ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
+        //ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
      /* arrayList.add(new SubjectData("JAVA", "https://www.tutorialspoint.com/java/images/java-mini-logo.jpg"));
       arrayList.add(new SubjectData("Python","https://www.tutorialspoint.com/python/images/python-mini.jpg"));
       arrayList.add(new SubjectData("Javascript", "https://www.tutorialspoint.com/javascript/images/javascript-mini-logo.jpg"));
@@ -51,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
       arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/images/android-mini-logo.jpg"));
 
       */
-        ListViewAdapter listViewAdapter = new ListViewAdapter(this, arrayList);
+        //ListViewAdapter listViewAdapter = new ListViewAdapter(this, arrayList);
         // list.setAdapter(listViewAdapter);
 
     }

c082b4a370c669ec35f9b6b5f72814ee1976b3d4
eortner18 <elias.ortner13@gmail.com>
2022-05-13 15:55:27 +0200

    Darkmode

--- app/src/main/java/com/example/pangerlular/SettingsActivity.java
+++ app/src/main/java/com/example/pangerlular/SettingsActivity.java
@@ -8,9 +8,11 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    boolean isDark = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,31 +23,53 @@ public class SettingsActivity extends AppCompatActivity {
         DBManager db = new DBManager();
 
         Button deleteAccount = findViewById(R.id.deleteAccount);
+        Button switch_btn = findViewById(R.id.switch_btn);
+            switch_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if (isDark == false) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        isDark = true;
+                    }
+                    else if (isDark == true) {
+
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        isDark = false;
+
+                    }
+                }
+            });
 
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        deleteAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                builder.setTitle("Account Löschen")
-                        .setMessage("Wollen Sie Ihren Account unwiederruflich löschen?")
-                        .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                db.deleteCustomer(LoginActivity.currentCustomer);
-                                restartFromLoginActivity(view);
-                            }
-                        })
-                        .setNegativeButton("Nein", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.cancel();
-                            }
-                        }).show();
 
-            }
-        });
-    }
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            deleteAccount.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    builder.setTitle("Account Löschen")
+                            .setMessage("Wollen Sie Ihren Account unwiederruflich löschen?")
+                            .setPositiveButton("Ja", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    db.deleteCustomer(LoginActivity.currentCustomer);
+                                    restartFromLoginActivity(view);
+                                }
+                            })
+                            .setNegativeButton("Nein", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.cancel();
+                                }
+                            }).show();
+
+                }
+            });
+
+
+        }
+
 
     public void restartFromLoginActivity(View view) {
         LoginActivity.currentCustomer = null;
